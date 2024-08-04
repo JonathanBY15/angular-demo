dad4a55241358b92d1f283fdbe94809c245ec7d5
JonathanBY15 <bariscanyaprak@gmail.com>
2024-08-04 10:32:50 -0400

    Added edit page and toggle button to modal

--- src/app/components/modal-button/modal-button.component.ts
+++ src/app/components/modal-button/modal-button.component.ts
@@ -1,12 +1,33 @@
+// import { Component } from '@angular/core';
+
+// @Component({
+//   selector: 'app-modal-button',
+//   standalone: true,
+//   imports: [],
+//   templateUrl: './modal-button.component.html',
+//   styleUrl: './modal-button.component.css'
+// })
+// export class ModalButtonComponent {
+
+// }
+
+
+
+
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-modal-button',
   standalone: true,
-  imports: [],
   templateUrl: './modal-button.component.html',
-  styleUrl: './modal-button.component.css'
+  styleUrls: ['./modal-button.component.css']
 })
 export class ModalButtonComponent {
+  isEditMode: boolean = false;
 
+  toggleEditMode(event: Event): void {
+    this.isEditMode = (event.target as HTMLInputElement).checked;
+  }
 }
+
+
